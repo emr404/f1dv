9e9dcf9ca26fd263879ccd35099cd2da934f9e37
emr404 <roufaym@gmail.com>
2020-09-06 22:58:57 +0100

    Set up routers

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React from 'react'
-
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Home from "./components/Home";
+import Nav from "./components/Navigation";
+import Drivers from "./components/Drivers";
+import Result from "./components/Result";
+import Standings from "./components/Standings";
+import Schedule from "./components/Schedule";
 const App = () => {
   return (
     <div>
-      
+      <Router>
+
+          <Nav />
+          <Switch>
+            <Route path='/' exact component={Home} />
+            <Route path='/Drivers' component={Drivers} />
+            <Route path='/Result' component={Result} />
+            <Route path='/Standings' component={Standings} />
+            <Route path='/Schedule' component={Schedule} />
+          </Switch>
+          
+      </Router>
     </div>
   )
 }
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,10 +1,28 @@
 import React from 'react'
-
+import { Link } from "react-router-dom";
 
 const Navigation = () => {
         return (
         <div>
+                <Link  to='/'> 
+                        <li>Home</li>
+                </Link>
+                
+                <Link  to='/Standings'> 
+                        <li>Standings</li>
+                </Link>
+                
+                <Link  to='/Schedule'> 
+                        <li>Schedule</li>
+                </Link>
 
+                <Link  to='/Result'> 
+                        <li>Results</li>
+                </Link>
+                
+                <Link  to='/Drivers'> 
+                        <li>Drivers</li>
+                </Link>
         </div>
         )
 }
