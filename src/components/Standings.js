f113ae0b263714085a8b4507e2ef13fc082d9593
emr404 <roufaym@gmail.com>
2020-09-07 18:58:36 +0100

    Added Standings component

--- src/components/Standings.js
+++ src/components/Standings.js
@@ -1,9 +1,107 @@
 import React from 'react'
+import { useState, useEffect } from "react";
+import { Bar, Line, Radar } from "react-chartjs-2";
+import StandingList from "./StandingsComponent";
+
 
 const Standings = () => {
+        const [chartData, setChartData] = useState([]);
+        const [raceSet, setRaceSet] = useState([]);
+        const [year, setYear] = useState(2020);
+        const [raceEvent, setRaceEvent] = useState([]);
+        const [seasonList, setSeasonList] = useState([]);
+        
+        
+        const chart = ()=>{
+        const chartLabel = raceEvent.map(driver => driver.name);
+        const chartDigits = raceEvent.map(driver => driver.points);
+        
+        setChartData({
+        labels: chartLabel,
+        datasets:[{
+        label:'Points',
+        data: chartDigits,
+        backgroundColor:'red',
+        borderColor:'black',
+        borderWidth:'0',
+        fill: false,
+        fontColor: 'black',
+        }]
+        ,
+        borderWidth:'20'
+    })
+}
+    useEffect(() => {
+        chart();
+    }, [raceEvent])
+        
+    
+    useEffect(() => {
+            const seasonArray = [];
+            fetch("http://ergast.com/api/f1/seasons.json?limit=100&offset=0")
+            .then((response) => response.json())
+            .then((data) => {
+                const realInfo = data.MRData.SeasonTable.Seasons;
+                realInfo.map((seasons) => {
+                const { season } = seasons;
+                seasonArray.push(season)
+                
+                });
+            });
+
+            setSeasonList(seasonArray);
+    }, [])
+    
+
+        useEffect(() => {
+            let ok = [];
+            fetch(`https://ergast.com/api/f1/${year}/driverStandings.json`)
+                .then((response) => response.json())
+                .then((data) => {
+                const moreData = data.MRData.StandingsTable.StandingsLists;
+                moreData.forEach((race) => {
+                    race.DriverStandings.map((driver) => {
+                    const {Driver: { permanentNumber, givenName, driverId,nationality, familyName },position,points,Constructors:[{name}]} = driver;
+                    ok.push({ name: familyName, fname:givenName, points: points,position:position, nationality:nationality, car: name, number:permanentNumber });
+                    });
+                });
+                setRaceEvent(ok);
+                });
+                
+            }, [year]);
+
+            const handleChange = e=>{
+                setYear(e.target.value);
+            }
     return (
-        <div>
-            <h1>Standings</h1>
+        <div className='StandingsContainer' >
+                <h1>Standings</h1>
+                <div className='Season'>
+                <h2>Season: {year}</h2>
+                <select value={year} onChange={handleChange} >
+                    <option value="">Select Year</option>
+                        {seasonList.map(season=>(<option key={season} value={season}>{season}</option>))}
+                </select>
+            </div>
+            <Line  data={chartData}/>
+            <div>
+
+            <div className='DriversListHeading'>
+                <ul>
+                    <li>
+                        <h3 style={{minWidth:'3vw'}} >Pos.</h3>
+                        <h3>Driver</h3>
+                        <h3>Constructor </h3>
+                        <h3 style={{minWidth:'3vw'}}>No.</h3>
+                        <h3>Points</h3>
+                    </li>
+                </ul>
+            </div>
+            {raceEvent.map(race => 
+                <StandingList key={race.position} {...race}/>
+            )}
+            </div>
+
         </div>
     )
 }
---
+++ src/components/StandingsComponent.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const StandingsComponent = ({position,points,name,fname,car,number}) => {
+    return (
+        <div>
+            <div>
+            <div className="DriversList">
+            <ul>
+                <li>
+                <h3 style={{minWidth:'3vw'}} >{position}</h3>
+                <h3> <strong> {fname} <br/> {name} </strong></h3>
+                <h3>{car}</h3>
+                <h3 style={{minWidth:'3vw'}}>{number}</h3>
+                <h3>{points}</h3>
+                </li>
+            </ul>
+            </div>
+        </div>
+        </div>
+    )
+}
+
+export default StandingsComponent
