0db16b7b5ed24e2899b6e1926ad0b5a9802e7643
emr404 <roufaym@gmail.com>
2020-09-07 19:22:09 +0100

    Added Results component

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,9 +1,126 @@
 import React from 'react'
+import {useEffect,useState} from 'react';
+import { Line } from "react-chartjs-2";
+import ResultComponent from "./ResultComponent";
+
 
 const Result = () => {
+    const [results, setResults] = useState([])
+    const [drivers, setDrivers] = useState([])
+    const [chartData, setChartData] = useState([])
+
+    const [year, setYear] = useState(2020)
+    const [seasonList, setSeasonList] = useState([])
+    const [round, setRound] = useState('last')
+    const [title, setTitle] = useState([])
+    
+    useEffect(() => {
+        fetch(`https://ergast.com/api/f1/${year}/${round}/results.json`)
+        .then(response=> response.json())
+        .then(data =>{
+            const resultsArray = [];
+            const driverArray = [];
+            const raceTitleArray=[];
+            
+            const raceData = data.MRData.RaceTable.Races;
+            raceData.map(race => {
+                console.log(race);
+                    const {raceName,date,season,round,Results,Circuit: {url,circuitName,Location: {locality,country}}} = race;
+                    /* console.log(raceName, date, season, 'Round ' + round); */
+                    raceTitleArray.push({race:raceName,date: date,season:season,round: round})
+                    Results.map(res => {
+                    const {Constructor:{name},position,points,Driver: {familyName,givenName,permanentNumber}} = res
+                        resultsArray.push({position:position,num:permanentNumber,car:name,  points:points,name:givenName,surname:familyName})
+                        driverArray.push({points:points, surname: familyName})
+                        /* console.log(position, permanentNumber, familyName, points, name); */
+                        })
+                        setResults(resultsArray);
+                        setDrivers(driverArray);
+                        setTitle(raceTitleArray)
+                })
+        })
+    }, [year,round])
+        const handleClick = e =>{
+        setRound(e.target.value)
+    }
+    const handleChange = e => {
+        setYear(e.target.value);
+    }
+useEffect(() => {
+    const seasonListArray = [];
+    fetch("http://ergast.com/api/f1/seasons.json?limit=100&offset=0")
+        .then((response) => response.json())
+        .then((data) => {
+            const seasonList = data.MRData.SeasonTable.Seasons;
+            seasonList.map((seasons) => {
+                const {season} = seasons;
+                seasonListArray.push(season)
+
+            });
+
+            setSeasonList(seasonListArray);
+        });
+
+    
+}, [])
+const handleSubmit = e => {
+    const driverName = drivers.map(driver => driver.surname)
+    const driverPoints = drivers.map(driver => driver.points)
+    setChartData({
+        labels: driverName,
+        datasets: [{
+            label: 'Points',
+            data: driverPoints,
+            backgroundColor: 'red',
+            borderColor: 'black',
+            borderWidth: '0',
+            fill: false,
+            fontColor: 'black',
+
+        }]
+
+    })
+}
+    
+    
     return (
         <div>
             <h1>Result</h1>
+            <select value={year} onChange={handleChange} >
+                <option value="">Select Season</option>
+                {seasonList.map(season=>(<option key={season} value={season}>{season}</option>))}
+            </select>
+            
+            <label htmlFor="Round"> Select Round</label>
+            <input type="number" min='1' onChange={handleClick} max='21' style={{border:'none', borderBottom:'1px solid red', width:'15vw', outline:'none'}} placeholder='Enter Round' />
+            
+            <button onClick={handleSubmit}>Click</button>
+
+
+
+            {title.map(race=>(
+                <h2>{race.race} <br/> Round:{race.round}</h2> 
+            ))}
+
+            <Line data={chartData} />
+
+            <div>
+                        <ul>
+                            <li>
+                                <h3 style={{minWidth:'3vw'}} >Pos.</h3>
+                                <h3>Driver</h3>
+                                <h3>Constructor</h3>
+                                <h3 style={{minWidth:'3vw'}}>No.</h3>
+                                <h3>Points</h3>
+                            </li>
+                        </ul>
+            </div>
+
+
+            {results.map(result=>(
+                        <ResultComponent key={result.position} {...result}/>
+            ))}
+
         </div>
     )
 }
---
+++ src/components/ResultComponent.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const ResultComponent = ({num,
+position,points, car, surname,name}) => {
+    return (
+        <div>
+            <div className="DriverCard">
+                <ul>
+                    <li>
+                        <h3 style={{minWidth:'3vw'}} >{position}</h3>
+                        <h3> <strong> {name} <br/> {surname} </strong></h3>
+                        <h3>{car}</h3>
+                        <h3 style={{minWidth:'3vw'}}>{num}</h3>
+                        <h3>{points}</h3>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default ResultComponent
